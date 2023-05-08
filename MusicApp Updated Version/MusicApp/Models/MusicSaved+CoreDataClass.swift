94d45f3019beb2cf7c307803c4234c0af870570e
JSPark0099 <91583287+JSPark0099@users.noreply.github.com>
2023-05-08 09:53:04 +0900

    [UIKit]뮤직앱

---
+++ MusicApp Updated Version/MusicApp/Models/MusicSaved+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  MusicSaved+CoreDataClass.swift
+//  MusicApp
+//
+//  Created by 박정선 on 2023/05/01.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(MusicSaved)
+public class MusicSaved: NSManagedObject {
+
+}
